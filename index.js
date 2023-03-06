cd93b9305e73005844509559bd1e9b0be17e67a0
MrChinmay03 <111864031+MrChinmay03@users.noreply.github.com>
2023-03-06 22:29:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    console.log("hello");
+})
+
 app.post("/",(req,res)=>{
     QRCode.toDataURL(req.body.qr_code_text, function (err, url) {
         console.log(url)
